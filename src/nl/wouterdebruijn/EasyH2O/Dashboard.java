9230ef088a801635abcf7e3d6c64c86f43c2d809
Emma van Eesteren <emmavaneesteren@gmail.com>
2020-11-19 10:32:02 +0100

    Test

--- src/nl/wouterdebruijn/EasyH2O/Dashboard.java
+++ src/nl/wouterdebruijn/EasyH2O/Dashboard.java
@@ -41,7 +41,11 @@ public class Dashboard {
         jFrame.pack();
         jFrame.setVisible(true);
     }
-
+/**
+ * print test tekst
+ */
+    System.out.println("UwU")
+    
     /**
      * Redraws the JFrame to be sized to components.
      */
